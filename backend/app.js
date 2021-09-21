9860535ec22a2220fff30a020daa07c813af37d5
unknown <baptiste.biancardino@gmail.com>
2021-09-21 19:38:50 +0200

    nouvel ajout d'user

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser =  require"body-parser"
+const bodyParser =  require("body-parser");
 const helmet = require('helmet');
 const path = require("path");
 require('dotenv').config();
